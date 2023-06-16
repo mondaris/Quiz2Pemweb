42d89c7d794f7e5f18d4c9e583ba8db27f36bfc8
mondaris <mohammadnafiis.19@gmail.com>
2023-06-16 21:05:35 +0700

    T

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Add Product</h1>
+    <h1>Tambahkan Produk</h1>
 
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -16,15 +16,15 @@
     <form action="{{ route('products.store') }}" method="POST">
         @csrf
         <div class="form-group">
-            <label for="name">Name</label>
+            <label for="name">Nama</label>
             <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
         </div>
         <div class="form-group">
-            <label for="price">Price</label>
+            <label for="price">Harga</label>
             <input type="number" class="form-control" id="price" name="price" value="{{ old('price') }}">
         </div>
         <div class="form-group">
-            <label for="stock">Stock</label>
+            <label for="stock">Stok</label>
             <input type="number" class="form-control" id="stock" name="stock" value="{{ old('stock') }}">
         </div>
         <button type="submit" class="btn btn-primary">Add</button>
